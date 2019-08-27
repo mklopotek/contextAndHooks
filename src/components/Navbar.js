18b60d4c6db770c50ab5643e77c848511a506efd
Magda Klopotek <magda.klopotek@gmail.com>
2019-08-27 18:56:26 +0200

    ThemeContext and AuthContext in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
+import { AuthContext } from '../context/AuthContext';
 
 //using Consumer
 const Navbar = (props) => {
 
 
     return (
-        <ThemeContext.Consumer>
+        <AuthContext.Consumer>
             {
-                (context) => {
-                    const { isLightTheme, light, dark } = context
-                    const theme = isLightTheme ? light : dark
+                (authContext) => (
+                    <ThemeContext.Consumer>
+                        {
+                            (themeContext) => {
+                                const { isLightTheme, light, dark } = themeContext
+                                const theme = isLightTheme ? light : dark
 
-                    return (
-                        <nav style={{ background: theme.ui, color: theme.syntax }}>
-                            <h1>Context App</h1>
-                            <ul>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
-                    )
-                }
+                                const { isAuthenticated, toggleAuth } = authContext
+
+                                return (
+                                    <nav style={{ background: theme.ui, color: theme.syntax }}>
+                                        <h1>Context App</h1>
+                                        <div onClick={toggleAuth}>
+                                            {isAuthenticated ? 'Hi! Logged in!' : 'Logged out!'}
+                                        </div>
+                                        <ul>
+                                            <li>Home</li>
+                                            <li>About</li>
+                                            <li>Contact</li>
+                                        </ul>
+                                    </nav>
+                                )
+                            }
+                        }
+                    </ThemeContext.Consumer>
+                )
             }
-        </ThemeContext.Consumer>
+        </AuthContext.Consumer>
     )
 
 }
