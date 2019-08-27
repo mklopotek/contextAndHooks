78de5b26f707cf565125e9b380ed1e8f9aa4bb08
Magda Klopotek <magda.klopotek@gmail.com>
2019-08-27 18:48:43 +0200

    create AuthContext

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from './components/Navbar'
 import BookList from './components/BookList';
 import ThemeContextProvider from './context/ThemeContext';
+import AuthContextProvider from './context/AuthContext';
 import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 
 //using Consumer
-class Navbar extends Component {
+const Navbar = (props) => {
 
-    render() {
 
-        return (
-            <ThemeContext.Consumer>
-                {
-                    (context) => {
-                        const { isLightTheme, light, dark } = context
-                        const theme = isLightTheme ? light : dark
+    return (
+        <ThemeContext.Consumer>
+            {
+                (context) => {
+                    const { isLightTheme, light, dark } = context
+                    const theme = isLightTheme ? light : dark
 
-                        return (
-                            <nav style={{ background: theme.ui, color: theme.syntax }}>
-                                <h1>Context App</h1>
-                                <ul>
-                                    <li>Home</li>
-                                    <li>About</li>
-                                    <li>Contact</li>
-                                </ul>
-                            </nav>
-                        )
-                    }
+                    return (
+                        <nav style={{ background: theme.ui, color: theme.syntax }}>
+                            <h1>Context App</h1>
+                            <ul>
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    )
                 }
-            </ThemeContext.Consumer>
-        )
-    }
+            }
+        </ThemeContext.Consumer>
+    )
+
 }
 
 export default Navbar
