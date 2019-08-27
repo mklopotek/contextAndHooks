a241519f60e4fd327c72694f55715445a4a04fae
Magda Klopotek <magda.klopotek@gmail.com>
2019-08-27 17:58:41 +0200

    cotext for theme

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { ThemeContext } from '../context/ThemeContext';
 
+//using this.context
 class BookList extends Component {
+    static contextType = ThemeContext
+
     render() {
-        return <div className={'book-list'}>
+
+        const { isLightTheme, light, dark } = this.context
+
+        const theme = isLightTheme ? light : dark
+
+        return <div className={'book-list'} style={{ color: theme.syntax, background: theme.bg }}>
             <ul>
-                <li>book 1 </li>
-                <li>book 2</li>
-                <li>book 3</li>
-                <li>book 4</li>
+                <li style={{ background: theme.ui }}>book 1 </li>
+                <li style={{ background: theme.ui }}>book 2</li>
+                <li style={{ background: theme.ui }}>book 3</li>
+                <li style={{ background: theme.ui }}>book 4</li>
             </ul>
         </div>
     }
