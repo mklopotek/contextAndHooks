a6e927e53edcac6d612057db8d369f61eb71eac4
Magda Klopotek <magda.klopotek@gmail.com>
2019-09-04 19:24:44 +0200

    add theme with useContext instead of this.context in BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 
 //using this.context
@@ -22,4 +22,20 @@ class BookList extends Component {
     }
 }
 
-export default BookList
+//using useContext hook
+const BookList2 = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext)
+
+    const theme = isLightTheme ? light : dark
+
+    return <div className={'book-list'} style={{ color: theme.syntax, background: theme.bg }}>
+        <ul>
+            <li style={{ background: theme.ui }}>book 1 </li>
+            <li style={{ background: theme.ui }}>book 2</li>
+            <li style={{ background: theme.ui }}>book 3</li>
+            <li style={{ background: theme.ui }}>book 4</li>
+        </ul>
+    </div>
+}
+
+export default BookList2
